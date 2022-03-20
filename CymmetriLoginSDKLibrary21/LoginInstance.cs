6ebe3ab18d8ea5d1cd5978e22a61f936b1904b8a
Abhishek Kulkarni <abhishek.kulkarni@unotechsoft.com>
2022-03-21 02:58:59 +0530

    Added Project

---
+++ CymmetriLoginSDKLibrary21/LoginInstance.cs
@@ -0,0 +1,59 @@
+﻿using CymmetriLoginSDKLibrary21.apicalls;
+using CymmetriLoginSDKLibrary21.utils;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+
+namespace CymmetriLoginSDKLibrary21
+{
+    public class LoginInstance
+    {
+        static HttpClient httpClient = new HttpClient();
+        public string baseUrl { get; set; }
+        public string tenant { get; set; }
+
+        //Example: baseUrl = https://box.cymmetri.in/
+        //Example: tenant = box
+        public LoginInstance(string baseUrl)
+        {
+            this.baseUrl = baseUrl;
+            // initializing as "", because we will get this directly from the domain validation API.
+            this.tenant = "";
+            httpClient.BaseAddress = new Uri(baseUrl);
+        }
+
+        //Gives encrypted Password that is analogous to crypto-js
+        public string GetEncryptedUser(string username, string password)
+        {
+            Protection p = new Protection();
+            var encryptedPassword = p.OpenSSLEncrypt(password, username);
+
+            return encryptedPassword;
+        }
+
+        // We already have the base URL, we should allow for calling this API as part of flow or independently
+        // We won't add this to the constructor, because the Internet may or may not be available during constructor call.
+        // CURL Request Example - 
+        /*
+         * curl 'https://demo191.cymmetri.in/regsrvc/custom-domain/validate' \
+            -H 'Accept: application/json' \
+            -H 'content-type: application/json' \
+            -H 'sec-ch-ua-platform: "Windows"' \
+            -H 'Origin: https://demo191.cymmetri.in' \
+            -H 'Sec-Fetch-Site: same-origin' \
+            -H 'Sec-Fetch-Mode: cors' \
+            -H 'Sec-Fetch-Dest: empty' \
+            -H 'Referer: https://demo191.cymmetri.in/' \
+            --data-raw '{"domain":"demo191.cymmetri.in"}' \
+            --compressed
+         */
+        public DomainValidationResponse ValidateDomain()
+        {
+            var domainValidation = new DomainValidation(httpClient);
+            var result = domainValidation.MakeRequest();
+            result.Wait();
+            DomainValidationResponse response = JsonConvert.DeserializeObject<DomainValidationResponse>(result.Result);
+            return response;
+        }
+    }
+}
---
+++ CymmetriLoginSDKLibrary21/apicalls/LoginValidation.cs
@@ -0,0 +1,86 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CymmetriLoginSDKLibrary21.apicalls
+{
+    class DomainValidationRequest
+    {
+        public DomainValidationRequest(string domain)
+        {
+            this.domain = domain;
+        }
+        public string domain { get; set; }
+    }
+
+    /*
+     * Success Response 
+     * {"success":true,"data":{"tenantId":"demo191","domain":"demo191.cymmetri.in"},"timestamp":"20-Mar-2022 07:20:47","message":null,"errorCode":null}
+     * 
+     * Failed Response
+     *  {"success":false,"data":null,"timestamp":"20-Mar-2022 07:21:24","message":null,"errorCode":"REGSRVC.UNAUTHORIZED"}
+     */
+    public class DomainValidationResponse
+    {
+        public bool success { get; set; }
+        public DomainValidationResponseData data { get; set; }
+        public string timestamp { get; set; }
+        public string message { get; set; }
+        public string errorCode { get; set; }
+    }
+
+    public class DomainValidationResponseData
+    {
+        public string tenantId { get; set; }
+        public string domain { get; set; }
+
+    }
+    class DomainValidation
+    {
+        public DomainValidation(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+            this.baseAddress = httpClient.BaseAddress;
+        }
+
+        public HttpClient httpClient { get; set; }
+        public Uri baseAddress { get; set; }
+        public StringContent GetJsonRequest()
+        {
+            var domain = baseAddress.Host;
+            var requestData = new DomainValidationRequest(domain);
+
+            var json = JsonConvert.SerializeObject(requestData);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+            return data;
+        }
+
+        public void SetClientHeaders()
+        {
+            var headerReferer = baseAddress.AbsoluteUri;
+            var headerOrigin = headerReferer.Remove(headerReferer.Length - 1, 1);
+            // Clear Headers
+            httpClient.DefaultRequestHeaders.Clear();
+
+            // Add appropriate Headers
+            httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
+            httpClient.DefaultRequestHeaders.Add("sec-ch-ua-platform", "Windows");
+            httpClient.DefaultRequestHeaders.Add("Origin", headerOrigin);
+            httpClient.DefaultRequestHeaders.Add("Sec-Fetch-Site", "same-origin");
+            httpClient.DefaultRequestHeaders.Add("Sec-Fetch-Mode", "cors");
+            httpClient.DefaultRequestHeaders.Add("Sec-Fetch-Dest", "empty");
+            httpClient.DefaultRequestHeaders.Add("Referer", headerReferer);
+
+        }
+
+        public async Task<string> MakeRequest()
+        {
+            var data = GetJsonRequest();
+            SetClientHeaders();
+            var response = await httpClient.PostAsync("regsrvc/custom-domain/validate", data);
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
